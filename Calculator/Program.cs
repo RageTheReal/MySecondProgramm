c6d17525e1074cee70612e3303436991dfe98853
Michael Jaeger <Jaegerle8@gmail.com>
2021-11-20 10:34:17 +0100

    MainMenu programmieren angefangen

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,32 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Hauptmenue: ");
+                Console.WriteLine("############");
+                Console.WriteLine("1) Addition");
+                Console.WriteLine("2) Subtraktion");
+                Console.WriteLine("#############");
+                Console.WriteLine("Deine eingabe:");
+
+
+
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }        
+        
         }
     }
 }
